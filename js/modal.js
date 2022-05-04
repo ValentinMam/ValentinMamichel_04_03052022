bfb89edb56828a19dd973c1f87e8c8aaa5b142d4
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2022-05-04 15:35:55 +0200

    regex and error alert inputs => firstName / lastName : OK

--- js/modal.js
+++ js/modal.js
@@ -38,3 +38,70 @@ closeModal(modalInscription, btnInscriptionClose);
 // END OF CLOSE MODAL FUNCTION
 
 // ISSUES #1 => OK
+
+// FORM
+
+// REGEX
+let regexFirstName = /^[a-zA-Z-\s]{2,}$/;
+let regexLastName = /^[a-zA-Z-\s]{2,}$/;
+
+// FIRSTNAME
+
+document.getElementById("firstName").addEventListener("input", function (e) {
+  validFirstName();
+});
+
+function validFirstName() {
+  const elt = document.forms["reserve"]["firstName"].value;
+  let errorFirstName = false;
+  let msgError = document.getElementById("errorFirstName");
+  let errorInputBorder = document.getElementById("firstName");
+
+  if (!elt) {
+    msgError.innerText = "Le champ ne doit pas être vide !";
+    errorInputBorder.style.border = "2px solid red";
+    errorFirstName = false;
+  } else if (!regexFirstName.test(elt)) {
+    msgError.innerText = "Veuillez entrer 2 caractères ou plus";
+    errorInputBorder.style.border = "2px solid red";
+    errorFirstName = false;
+  } else if (regexFirstName.test(elt)) {
+    msgError.innerText = "";
+    errorInputBorder.style.border = "none";
+    errorFirstName = true;
+  } else {
+    errorFirstName = true;
+  }
+  return errorFirstName;
+}
+
+// LASTNAME
+
+document.getElementById("lastName").addEventListener("input", function (e) {
+  validLast();
+});
+
+function validLast() {
+  let elt = document.forms["reserve"]["lastName"].value;
+  let errorLastName = false;
+
+  let msgError = document.getElementById("errorLastName");
+  let errorInputBorder = document.getElementById("lastName");
+
+  if (!elt) {
+    msgError.innerHTML = "Le champ ne doit pas être vide !";
+    errorInputBorder.style.border = "2px solid red";
+    errorLastName = false;
+  } else if (!regexLastName.test(elt)) {
+    msgError.innerHTML = "Veuillez entrer 2 caractères ou plus";
+    errorInputBorder.style.border = "2px solid red";
+    errorLastName = false;
+  } else if (regexLastName.test(elt)) {
+    msgError.innerHTML = "";
+    errorInputBorder.style.border = "none";
+    errorLastName = true;
+  }
+  return errorLastName;
+}
+
+// SUBMIT
